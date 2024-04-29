c6d6477d81e6be9d2183344729a8c763585a9358
ᴅᴀɴɪᴇʟ ʏᴏᴏɴ <36707871+wnsdbs@users.noreply.github.com>
2024-04-29 09:23:51 -0400

    Update NetflixMovieList.java

--- NetflixMovieList.java
+++ NetflixMovieList.java
@@ -1,25 +1,20 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class NetflixMovieList
-{
+public class NetflixMovieList {
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		// Set-up
 		Scanner s = new Scanner(System.in);
 		ArrayList<String> list = new ArrayList<String>();
 		Movie.main(args);
-		
 		// Create ArrayList with Movie Info. using Movie class
-		for (int i = 0; i < Movie.mov.size(); ++i)
-		{
+		for (int i = 0; i < Movie.mov.size(); ++i) {
 			list.add(Movie.mov.get(i));
 		}
-		
 		// Stores user-input name and movie amount
 		String name = "";
 		int amount = 0;
-		
 		// Prompts User
 		System.out.print("Welcome to Netflix!\nEnter your name: ");
 		name = s.next();
@@ -28,35 +23,25 @@ public class NetflixMovieList
 		System.out.println("-----------------");
 		System.out.println("Welcome, " + name + "!");
 		System.out.println("-----------------");
-		
 		// Prints out the number of movies the user desired
-		if (amount > 5)
-		{
+		if (amount > 5) 
 			System.out.print("Error: Try Again!");
-		}
-		else 
-		{			
-			repeat(list, amount);	
-		}	
+		else 		
+			repeat(list, amount);		
 	}
 
-	public static void repeat(ArrayList<String> list, int amount)
-	{
+	public static void repeat(ArrayList<String> list, int amount) {
 		// Set-up
 		Scanner s = new Scanner(System.in);
 		ArrayList<String> fav = new ArrayList<String>();
 		int loop = 0;
-			
 		// While-loop for infinite prompts until user inputs "X"
-		while (loop == 0)
-		{
+		while (loop == 0) {
 			// Prompts user the option menu
 			System.out.print("Movie Options:\n");	
 			System.out.println("-----------------");
-			for (int i = 1; i <= amount; i++)
-			{		
+			for (int i = 1; i <= amount; i++)	
 				System.out.println(i + ".  " + list.get(i - 1));
-			}
 			System.out.println("-----------------");
 			System.out.println("Select an Option:");
 			System.out.println("> to scroll forward");
@@ -71,10 +56,8 @@ public class NetflixMovieList
 			System.out.print("Please enter your selection: ");	
 			// Stores user-input
 			String check = s.next();
-			
 			// If-statements for each command when inputed			
-			if (check.equals("D"))
-			{		
+			if (check.equals("D")) {		
 				// Displays all 5 movie selections
 				for (int i = 1; i <= list.size(); i++)
 				{
@@ -82,32 +65,23 @@ public class NetflixMovieList
 				}
 				System.out.println("------------------");	
 			}
-			
 			// Displays all favorite selections						
-			if (check.equals("F"))
-			{		
+			if (check.equals("F")) {		
 				System.out.println("Loading Favorites...");
-				for (int i = 1; i <= fav.size(); ++i)
-				{
+				for (int i = 1; i <= fav.size(); ++i) 
 					System.out.println(i + ".  " + fav.get(i - 1));
-				}
 				System.out.println("------------------");	
 			}			
-			
-			if (check.equals("M"))
-			{		
+			if (check.equals("M")) {		
 				// Prompts user to select a movie (1-5) 
 				System.out.print("What movie would you like to select: "); 
 				int movie = s.nextInt();
 				System.out.println("------------------"); 
 				System.out.println(list.get(movie - 1)); 
 				// Searches for the respective movie's description
-				for (int i = 1; i <= Movie.des.size(); ++i)
-				{
+				for (int i = 1; i <= Movie.des.size(); ++i) {
 					if (movie == i)
-					{
 						System.out.println(Movie.des.get(i - 1)); 
-					}
 				}
 				System.out.println("-----------------");
 				System.out.println("Select an Option:");
@@ -118,32 +92,26 @@ public class NetflixMovieList
 				System.out.println("------------------");	
 				System.out.print("Please enter your selection: ");	
 				String check2 = s.next();		
-				if (check2.equals("F"))
-				{
+				if (check2.equals("F")) {
 					// Adds to Favorite List, stores the respective movie to the ArrayList
 					System.out.print(Movie.mov.get(movie - 1) + ", Added to Favorites!\n");
 					fav.add(Movie.mov.get(movie-1));
 					System.out.println("------------------");	
 				}
-				if (check2.equals("W"))
-				{
+				if (check2.equals("W")) {
 					// Plays the respective movie chosen by user
 					System.out.print("Loading...\n\n");
 					System.out.print("Enjoy, " + list.get(movie - 1) + "!");
 					break;
 				}
-				if (check2.equals("R"))
-				{
+				if (check2.equals("R")) {
 					// Redirects user back to Movie List and options
 					System.out.println("Redirecting to Movie List...\nMovie Options:");
-					for (int i = 1; i <= amount; i++)
-					{		
+					for (int i = 1; i <= amount; i++)		
 						System.out.println(i + ".  " + list.get(i - 1));
-					}
 					System.out.println("------------------");	
 				}
-				if (check2.equals("X"))
-				{
+				if (check2.equals("X")) {
 					// If inputed, end of program and stop loop
 					System.out.print("Program Exited!");
 					break;	
@@ -151,8 +119,7 @@ public class NetflixMovieList
 			}		
 			
 			// Custom user input of a movie
-			if (check.equals("A"))
-			{							
+			if (check.equals("A")) {							
 				Scanner f = new Scanner(System.in);
 				System.out.print("Movie Name: ");
 				String movName = f.nextLine();
@@ -171,42 +138,30 @@ public class NetflixMovieList
 				list.add(t);	
 				System.out.println("------------------");	
 			}		
-			
 			// Removes desired movie from the list
-			if (check.equals("R"))
-			{
+			if (check.equals("R")) {
 				int amountOfMovies = list.size();
 				System.out.print("Select movie to remove (1-" + amountOfMovies + "): ");
 				list.remove(s.nextInt()-1);
 				System.out.println("------------------");	
 			}	
-		
-			if (check.equals(">"))
-			{		
+			if (check.equals(">")) {		
 				// Scrolls forward once
 				String temp = list.get(list.size() - 1);
 				for (int i = list.size() - 1; i > 0; i--) 
-				{
 					list.set(i, list.get(i - 1));
-				}
 				list.set(0, temp);
 				System.out.println("------------------");	
 			}	
-			
-			if (check.equals("<"))
-			{		
+			if (check.equals("<")) {		
 				// Scrolls backward once
 				String temp = list.get(0);
 				for(int i = 0; i < list.size() - 1; i++) 
-				{
 				    list.set(i, list.get(i + 1));
-				}
 				list.set(list.size() - 1, temp);
 				System.out.println("------------------");	
 			}	
-				
-			if (check.equals("X"))
-			{
+			if (check.equals("X")) {
 				// If inputed, end of program and stop loop
 				System.out.print("Program Exited!");
 				break;	
